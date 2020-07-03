dc339e2b82ea3c0ddcbb319598c7dd17aad43796
adybaby <52179843+adybaby@users.noreply.github.com>
2020-07-03 02:05:34 +0100

    fixing bug in error handling

--- src/state/actions/data/server/ServerInterface.js
+++ src/state/actions/data/server/ServerInterface.js
@@ -19,55 +19,66 @@ export const ACTIONS = {
   FIND: 'find',
 };
 
-const error = (err) => `Could not query the server: ${err}`;
+const noQueryMsg = (err = 'Unknown') =>
+  `There was a problem executing the query. Error: ${err.message}`;
+const noServerMsg = (err = '404') =>
+  `There was a problem contacting the TaskMaster server. Is it running? Error: ${err.message}`;
 
 export const query = (action, entity, updateOrQuery) =>
   new Promise((resolve, reject) => {
-    fetch(QUERY_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ action, entity, updateOrQuery }),
-    }).then((res) => {
-      switch (res.status) {
-        case 200:
-          try {
-            res
-              .json()
-              .then((data) => {
-                resolve(stringDatesToRealDates(data));
-              })
-              .catch((e) => {
-                reject(error(e.message));
-              });
-          } catch (e) {
-            reject(error(e.message));
+    try {
+      fetch(QUERY_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ action, entity, updateOrQuery }),
+      })
+        .then((res) => {
+          switch (res.status) {
+            case 200:
+              try {
+                res
+                  .json()
+                  .then((data) => {
+                    resolve(stringDatesToRealDates(data));
+                  })
+                  .catch((e) => {
+                    reject(noQueryMsg(e));
+                  });
+              } catch (e) {
+                reject(noQueryMsg(e));
+              }
+              break;
+
+            case 404:
+              reject(noServerMsg());
+              break;
+
+            case 500:
+              try {
+                res
+                  .json()
+                  .then((msg) => {
+                    reject(noQueryMsg(new Error(msg)));
+                  })
+                  .catch((e) => {
+                    reject(noQueryMsg(e));
+                  });
+              } catch (e) {
+                reject(noQueryMsg(e));
+              }
+              break;
+
+            default:
+              reject(noQueryMsg());
+              break;
           }
-          break;
-
-        case 404:
-          reject(new Error('The Taskmaster server could not be found. Is it running?'));
-          break;
-
-        case 500:
-          try {
-            res
-              .json()
-              .then((msg) => {
-                reject(error(msg));
-              })
-              .catch((e) => {
-                reject(error(e.message));
-              });
-          } catch (e) {
-            reject(error(e.message));
-          }
-          break;
-
-        default:
-          reject(error('Unknown error when querying server.'));
-          break;
-      }
-    });
+        })
+        .catch((err) => {
+          reject(noServerMsg(err));
+        });
+    } catch (err) {
+      reject(noServerMsg(err));
+    }
   });
